29f0672b522c45640e900f84033f56a034c643d2
jlaguilargomez <jlaguigom@gmail.com>
2020-03-26 10:46:06 +0100

    section 1--9

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { interval, fromEvent, Observable, noop } from 'rxjs';
+import { map, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  styleUrls: ['./about.component.css'],
 })
 export class AboutComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
-  }
+    const http$ = Observable.create(observer => {
+      fetch('/api/courses')
+        .then(response => {
+          return response.json();
+        })
+        .then(body => {
+          observer.next(body);
+          observer.complete();
+        })
+        .catch(err => {
+          observer.error(err);
+        });
+    });
 
+    http$.subscribe(
+      courses => console.log(courses),
+      noop,
+      () => console.log('completed')
+    );
+  }
 }

82a3f5638fd4742b9aa16edd9d6aff7273064a45
jlaguilargomez <jlaguigom@gmail.com>
2020-03-26 17:14:06 +0100

    section 2--15

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { interval, fromEvent, Observable, noop } from 'rxjs';
+import { interval, fromEvent, Observable, noop, of, concat } from 'rxjs';
 import { map, filter } from 'rxjs/operators';
 import { createHttpObservable } from '../common/util';
 
@@ -11,5 +11,15 @@ import { createHttpObservable } from '../common/util';
 export class AboutComponent implements OnInit {
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    const source1$ = of(1, 2, 3);
+
+    const source2$ = of(4, 5, 6);
+
+    const source3$ = of(7, 8, 9);
+
+    const result$ = concat(source1$, source2$, source3$);
+
+    result$.pipe(map(val => val + 1)).subscribe(console.log);
+  }
 }
